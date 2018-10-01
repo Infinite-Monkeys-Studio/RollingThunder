b0d3cbaf5ff9f134ba8424cd34111583a69218a9
Qhenckel <boardman94@gmail.com>
2018-10-01 14:14:12 -0500

    Working on rendering for inside vs outside cameras

---
+++ Assets/Scripts/Extirior_Overlay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extirior_Overlay : MonoBehaviour {
+
+	public RenderTexture outside;
+	public Texture sky;
+	public bool render_background;
+	public CameraClearFlags inside_clearflags;
+	public CameraClearFlags outside_clearflags;
+
+	private Camera cam;
+
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (render_background) {
+			cam.clearFlags = inside_clearflags;
+		} else {
+			cam.clearFlags = outside_clearflags;
+		}
+	}
+
+	void OnGUI() {
+		if (render_background) {
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), sky);
+
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), outside);
+
+			cam.Render ();
+		}
+	}
+}
