96dfe2fe1dc7e7cdebe06beed9c6d26d269d66ec
Qhenckel <boardman94@gmail.com>
2018-10-06 12:53:44 -0500

    finish gun control and shooting

--- Assets/Scripts/Control/Gun_Controller.cs
+++ Assets/Scripts/Control/Gun_Controller.cs
@@ -6,10 +6,16 @@ public class Gun_Controller : Controller {
 
 	public Transform ExitPosition;
 	public Transform UsingPosition;
+	public float fireRate;
+	public Transform Barrel;
+	public GameObject bullet;
+	public float bulletSpeed;
 
 
 	Transform player;
-
+	bool firstTime = true;
+	bool shooting = false;
+	float fireDelay = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -18,19 +24,61 @@ public class Gun_Controller : Controller {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Interact")) {
-			//organizer.switchTo(player.GetComponent<Controller>().name);
+		if (active) {
+			if(Input.GetButtonDown("Interact")) {
+				if(firstTime) {
+					firstTime = false;
+				} else {
+					organizer.switchTo(player.GetComponent<Controller>().name);
+				}
+			}
+
+			if(Input.GetButtonDown("Fire1")) {
+				shooting = true;
+			}
+
+			if(Input.GetButtonUp("Fire1")) {
+				shooting = false;
+			}
+
+			Vector3 mousePosistion = Input.mousePosition;
+			mousePosistion = Camera.main.ScreenToWorldPoint(mousePosistion);
+
+			Vector2 direction = new Vector2(mousePosistion.x - transform.position.x, mousePosistion.y - transform.position.y);
+
+			transform.right = direction;
 		}
+	}
+
+	void FixedUpdate() {
+		if (shooting) {
+			if (fireDelay > fireRate) {
+				fire();
+				fireDelay = 0;
+			}
+		}
+
+		fireDelay += Time.fixedDeltaTime;
+	}
+
+	void fire() {
+		GameObject b = GameObject.Instantiate (bullet, Barrel.position, Barrel.rotation);
+		b.GetComponent<Rigidbody2D>().AddForce(b.transform.up.normalized * bulletSpeed);
+
 	}
 
 	public override void OnSwitchTo() {
 		player.position = UsingPosition.position;
-		player.SetParent (transform);
+		GameObject go = player.gameObject;
+		go.SetActive (false);
 		active = true;
 	}
 
 	public override void OnSwitchFrom() {
 		player.position = ExitPosition.position;
+		GameObject go = player.gameObject;
+		go.SetActive (true);
 		active = false;
+		firstTime = true;
 	}
 }
