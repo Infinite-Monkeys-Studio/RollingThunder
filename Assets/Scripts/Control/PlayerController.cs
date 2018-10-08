752be45f78a1ce6d219e71b9f86cde91c4dd2d20
Qhenckel <boardman94@gmail.com>
2018-10-07 22:00:50 -0500

    Added new Player charicter and started work on better player control

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -5,30 +5,28 @@ using UnityEngine;
 public class PlayerController : Controller {
 
 	public CharacterController2D controller;
+	public float MovementSpeed = 10f;
+	public Animator animator;
 
 	float horizontalMove = 0f;
 	bool jump = false;
-	bool crouch = false;
+
 
 	void Update() {
 		if (active) {
 			horizontalMove = Input.GetAxisRaw ("Horizontal");
+			animator.SetFloat("Speed", Mathf.Abs (horizontalMove));
 	
 			if (Input.GetButtonDown ("Jump")) {
 				jump = true;
-			}
-
-			if (Input.GetAxisRaw ("Vertical") < -0.5f) {
-				crouch = true;
-			} else {
-				crouch = false;
+				animator.SetTrigger("Jump");
 			}
 		}
 	}
 
 	void FixedUpdate() {
 		if (active) {
-			controller.Move (horizontalMove, crouch, jump);
+			controller.Move (horizontalMove * MovementSpeed, jump);
 			jump = false;
 		}
 	}
